974be23666ded2c2acefaf97517d25392342ba3e
AN <al.gribovsky@gmail.com>
2024-05-20 23:33:19 +0300

    Удалил все комментарии

--- IOS13-HW10/ViewController.swift
+++ IOS13-HW10/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  IOS13-HW10
-//
-//  Created by Alexey Gribovsky on 20.05.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     func setupView() {
