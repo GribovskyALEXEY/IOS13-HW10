bb6b32f60d1bb524acbbb95760ec3689a27546ac
AN <al.gribovsky@gmail.com>
2024-05-20 22:41:12 +0300

    Initial Commit

---
+++ IOS13-HW10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS13-HW10
+//
+//  Created by Alexey Gribovsky on 20.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
