d1229da9872ce5a2947c5c8842224f4c245542c7
AN <al.gribovsky@gmail.com>
2024-05-22 22:51:42 +0300

    Revert "Удалил все комментарии"
    This reverts commit 974be23666ded2c2acefaf97517d25392342ba3e.

--- IOS13-HW10/ViewController.swift
+++ IOS13-HW10/ViewController.swift
@@ -1,9 +1,17 @@
+//
+//  ViewController.swift
+//  IOS13-HW10
+//
+//  Created by Alexey Gribovsky on 20.05.2024.
+//
+
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
     }
 
     func setupView() {
